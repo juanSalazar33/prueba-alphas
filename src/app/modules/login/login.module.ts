e16de14628d8a4d14d7b8d2a690a83825969a5fa
Juan José <juancrogollo@gmail.com>
2022-11-11 14:52:58 -0400

    set angular proyect, create login module

---
+++ src/app/modules/login/login.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginRoutingModule } from './login.routing';
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule 
+  ]
+})
+export class LoginModule { }
